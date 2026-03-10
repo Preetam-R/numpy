3fb4784aa916d458a7db3eeca7a955450b4be8f6
Preetam Rathod <rathodpreetam613@gmail.com>
2026-03-10 22:30:27 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,9 @@
+#Create a 3×3 array filled with zeros and a 3×3 array filled with ones.
+
+import numpy as np
+print("The 3X3 zero array:")
+zero = np.zeros((3,3))
+print(zero)
+print("The 1X1 ones array:")
+one = np.ones((3,3))
+print(one)
