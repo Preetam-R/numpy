88c18cc04018c538a9bf90d5ab197dce0aa60cc1
Preetam Rathod <rathodpreetam613@gmail.com>
2026-03-10 22:28:21 +0530

    Add files via upload

---
+++ nmp.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.arange(0,11)
+print(arr)
+
+print("--"*30)
+
+arn = np.array([1,2,3,4,5],ndmin = 10)   #this creates multi-dimensional array
+print(arn)
+print(arn.ndim)
