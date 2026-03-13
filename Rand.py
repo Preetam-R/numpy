d1e881d5b0f1ef075c8701d3afce4aae565321dd
Preetam Rathod <rathodpreetam613@gmail.com>
2026-03-13 22:28:12 +0530

    Add files via upload

---
+++ Rand.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.random.rand(4)  #this rand() give number between 0 and 1
+print(arr)   #output: [0.82656712 0.21457894 0.05117164 0.8029551 ]
+
+#for multi-dimension 
+arr1 = np.random.rand(3,3)
+print(arr1)
